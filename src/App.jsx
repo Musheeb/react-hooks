1072a85b7cb71c4f6e814c5c1fd944567a623ad5
Musheeb Affan <musheebaffan@Musheebs-MacBook-Air.local>
2026-02-06 01:50:54 +0530

    feat: useReducer practiced

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  function reducerFunc(state, action) {
+    switch (action.type) {
+      case "increment":
+        return { ...state, count: state.count + 1 };
+      case "decrement":
+        return { ...state, count: state.count - 1 };
+      case "thoughts":
+        return { ...state, thoughts: action.payload };
+      case "color":
+        return { ...state, isColor: !state.isColor };
+      default:
+        throw new Error("Error occured in reducer method");
+    }
+  }
+  const [state, dispatch] = useReducer(reducerFunc, {
+    count: 0,
+    thoughts: "",
+    isColor: false,
+  });
+  // console.log("state is - ", state);
+  // const [count, setCount] = useState(0);
+  // const [thoughts, setThoughts] = useState("");
+  // const [isColor, setIsColor] = useState(false);
+
+  function handleOnChange(event) {
+    // setThoughts(event.target.value);
+    dispatch({ type: "thoughts", payload: event.target.value });
+  }
+
+  const styles = {
+    color: !state.isColor ? "#bbb" : "green",
+  };
 
   return (
     <>
@@ -18,9 +49,33 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <input
+          type="text"
+          className="input--text"
+          placeholder="Write your thoughts..."
+          onChange={handleOnChange}
+          value={state.thoughts}
+        />
+        <button
+          className="button--subtract"
+          onClick={() => dispatch({ type: "decrement" })}
+        >
+          Subtract count by 1
         </button>
+        <button onClick={() => dispatch({ type: "increment" })}>
+          count is {state.count}
+        </button>
+        {state.thoughts && (
+          <p
+            className="text--paragraph"
+            style={styles}
+            onClick={() => {
+              dispatch({ type: "color" });
+            }}
+          >
+            {state.thoughts}
+          </p>
+        )}
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
