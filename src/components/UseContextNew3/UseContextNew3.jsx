815b2f43a5632d569820f8bf81eb0e5ab32e10cc
Musheeb Affan <musheebaffan@Musheebs-MacBook-Air.local>
2026-02-14 00:39:41 +0530

    feat: new use memo example

--- src/components/UseContextNew3/UseContextNew3.jsx
+++ src/components/UseContextNew3/UseContextNew3.jsx
@@ -1,16 +1,44 @@
 import "./UseContextNew3.css";
 
 import { CreatedContext } from "../ContextProvider1/context.js";
-import { useContext } from "react";
+import { useContext, useState, useMemo } from "react";
+
+const fib = (n) => {
+  return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+};
 
 export default function UseContextNew3() {
+  console.log("Child component re-rendered");
+  const [myNumber, setMyNumber] = useState("");
+  const [myText, setMyText] = useState("");
   const { username, age, profession } = useContext(CreatedContext);
+  const fibNumber = useMemo(() => fib(myNumber), [myNumber]);
+  function handleFibNumber(e) {
+    setMyNumber(Number(e.target.value));
+  }
+  function handleTextChange(e) {
+    setMyText(e.target.value);
+  }
   return (
     <div className="use__new__context__3--container">
       <h2>Context new 3 container</h2>
       <p>Name: {username}</p>
       <p>Age: {age}</p>
       <p>Profession: {profession}</p>
+      <input
+        type="number"
+        placeholder="enter you number..."
+        value={myNumber}
+        onChange={handleFibNumber}
+      />
+      <p>{fibNumber ? fibNumber : "--"}</p>
+      <input
+        type="text"
+        placeholder="enter something..."
+        value={myText}
+        onChange={handleTextChange}
+      />
+      <p>{myText ? myText : "--"}</p>
     </div>
   );
 }
